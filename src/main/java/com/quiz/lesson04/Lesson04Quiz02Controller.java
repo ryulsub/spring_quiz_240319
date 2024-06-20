a1e2035aacea0cc556743b41a0cfeb0ddf83ee0b
DESKTOP-E4VTSLN\user <test@test.com>
2024-06-20 18:50:57 +0900

    240620

---
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz02Controller.java
@@ -0,0 +1,48 @@
+package com.quiz.lesson04;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.quiz.lesson04.bo.RealtorBO;
+import com.quiz.lesson04.domain.Realtor;
+
+@RequestMapping("/lesson04/quiz02")
+@Controller
+public class Lesson04Quiz02Controller {
+	
+	@Autowired
+	private RealtorBO realetorBO;
+	
+	// 입력화면
+	// http://localhost:8080/lesson04/quiz02/add-realtor-view
+	@GetMapping("//add-realtor-view")
+	public String addRealtorView() {
+		return "lesson04/addRealtor";
+	}
+	
+	// DB Insert, select
+	@PostMapping("/add-realtor")
+	public String addRealtor(
+			@ModelAttribute Realtor realtor,
+			Model model) {
+	
+		// DB insert
+		realtorBO.addRealtor(realtor);
+		
+		// id를 가지고 DB select
+		Realtor latestRealtor = realtorBO.getRealtorById(realtor.getId());
+		
+		// Model에 데이터 담기
+		model.addAttribute("realtor", latestRealtor);
+		
+		// 화면 이동
+		return "lesson04/afterAddRealtor";
+	}
+	
+	
+}
